17e8fd35e0e33655ef209250aa9394c9c44ec9a5
Highzik <ajadiisaac99@gmail.com>
2023-12-26 15:22:55 +0100

    I modified the html and css page and created a components for the project and image folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import Home from "./components/Home"
+import '/src/index.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import { useState } from "react"
 
 function App() {
-
+  const [data, setData] = useState([
+    { id: 1, task: 'Bogiloli suit fitting', place: 'Linnegotan 2, Gothenburg', time: '9am' },
+    { id: 2, task: 'Final design delivery', place: 'Inuse Experience, Lagos Island', time: '11am' },
+    { id: 3, task: 'Lunch run meeting', place: 'Victoria Garden City VGC', time: '12am' },
+    { id: 4, task: 'Record techno mixtape', place: 'Klockwork Studios, Maryland', time: '7pm' },
+  ])
   return (
-    <>
-    </>
+    <div className="container">
+      <Home />
+    </div>
   )
 }
 
---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import '/src/index.css'
+import img1 from '/src/images/coast-8308438_1280.jpg'
+const Home = () => {
+  return (
+    <div>
+      <img src={img1} alt="" />
+      <p>INBOX</p>
+      <p>This is the home page</p>
+    </div>
+  )
+}
+
+export default Home
